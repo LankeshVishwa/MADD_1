c426f1011adc634f1cfd2aab29d046c5763bc59b
Dell <lankeshmahanthegama7@gmail.com>
2021-09-18 22:51:35 +0530

    MADD project interface_1

---
+++ app/src/main/java/IT20162764/Interface_1.java
@@ -0,0 +1,16 @@
+package IT20162764;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cupcakes.kandycupcakes.R;
+
+public class Interface_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interface1);
+    }
+}
